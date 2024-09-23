4c7e702c4f3056ef27c29f122419610cc40c2fd8
Joao <joao.cassatti@movemais.com>
2024-09-23 14:26:10 -0300

    acertando baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://foodexplorer-api-z0vq.onrender.com/'
+  baseURL: 'https://foodexplorer-api-z0vq.onrender.com'
 });
